411c57e443875b6da32c2bc0fac8c1a452678618
Daniel R <nzdanron@gmail.com>
2023-04-06 19:56:50 +1200

    Changed prompt for creating a .tasks.md file
    Updated the prompt to be a simple y/n keypress, in the interest of
    speed. Since users can easily change the title of the list, having a
    default name isn't so bad. I haven't extensively tested it but I think it
    works just fine. It does use some crossterm libraries, so I think I
    might have to move the code around in the interest of separating
    concerns. It does cleanup after itself though?

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,24 @@ mod app;
 
 use app::{deserialise, new_tasks_data, serialise, TasksApp};
 
+use crossterm::event::{read, Event, KeyCode};
+
 fn main() {
     let lists = match deserialise() {
         Ok(lists) => lists,
         Err(_) => {
-            println!("Coudln't find a .tasks.md file in this directory, Provide a name for the first list:");
-            let mut list_name = String::new();
-            std::io::stdin()
-                .read_line(&mut list_name)
-                .expect("Failed to read line");
-            new_tasks_data(list_name.trim())
+            println!("Coudln't find a .tasks.md file in this directory, create one? (Y/n)");
+            crossterm::terminal::enable_raw_mode().expect("Failed to enable raw terminal mode");
+            match read().expect("failed to read") {
+                Event::Key(key) => match key.code {
+                    KeyCode::Char('n') | KeyCode::Char('N') => return,
+                    _ => new_tasks_data("Main"),
+                },
+                _ => new_tasks_data("Main"),
+            }
         }
     };
+    crossterm::terminal::disable_raw_mode().expect("Failed to disable raw terminal mode");
 
     let mut app = TasksApp::new(lists);
 
